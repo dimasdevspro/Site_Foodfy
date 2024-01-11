6e55c436b98b9cfc1f063c42d0ef1b802331ef16
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-01-11 15:06:56 -0300

    Adjusted src by modaOverlay in image

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ for (let card of cards) {
     card.addEventListener("click", function(){
         const imgId = card.getAttribute("id")
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector("img").src =`/assets/${imgId}`
+        modalOverlay.querySelector("img").src =`./assets/${imgId}`
 
         const title = card.querySelector('.title_revenue p')
         modalOverlay.querySelector(".modal-content .title_revenue p").textContent=title.textContent
